f644d3547a780aa1052e9a1d236c3dcfcfac39c6
Hoshi <91725235+hoshi2317@users.noreply.github.com>
2024-07-22 07:26:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,42 +1,32 @@
-//base by DGXeon
-//re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
-
-const fs = require('fs')
 const chalk = require('chalk')
 
 //session
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT:-" //ur yt chanel name
+global.socialm = "Github hoshi2317" //ur github or insta name
+global.location = "Indonesia, jakarta, bandung" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.ownernumber = '6283159699851' //ur owner number
+global.ownername = 'hoshi' //ur owner name
+global.websitex = "-"
+global.wagc = "-"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.botscript = 'https://github.com/hoshi2317/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "hoshi\n\n+6283159699851"
+global.creator = "6283159699851@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["6283159699851"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
-global.typereply = 'v2' // reply type 'v1' => 'v3'
+global.typereply = 'v3' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
@@ -46,7 +36,7 @@ global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
 global.mess = {
-	limit: 'Your limit is up!',
+	limit: 'limit mu sudah habis!',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
     done: 'Done✓',
     error: 'Error!',
